74cc60defb84dcc088de031cc4aaa92fc7255eee
sebastienwood <sebastien.henwood@gmail.com>
2016-04-11 17:13:52 +0200

    Base methods to check compatibility

--- src/Telescope.java
+++ src/Telescope.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 
 /**
  * @author Sébastien
- *
+ * A class to represent a telescope schedule
  */
 public class Telescope {
 
@@ -16,11 +16,34 @@ public class Telescope {
 	/**
 	 * Add an observation to the planning
 	 * @param o: the observation to add
-	 * @return false if the observation is incompatible
 	 */
-	public boolean add(Plan p) {
-		//TODO: process of adding a plan in the telescope schedule with review of all plan already on schedule
-		return false;
+	public void add(Plan p) {
+		this.planning.add(p);
+	}
+	
+	/**
+	 * A method to check if the current schedule is valid
+	 * Check:
+	 * -same star not twice
+	 * -all plan are compatible with each other
+	 * @return true if the schedule is valid
+	 */
+	public boolean isValid() {
+		boolean valid = true;
+		Iterator<Plan> i = planning.iterator();
+		while(i.hasNext()) {
+			Plan p = i.next();
+			Iterator<Plan> i2 = planning.iterator();
+			while(i2.hasNext()) {
+				Plan p2 = i2.next();
+				if(p.getStar() == p2.getStar() || !p.isCompatible(p2)) {
+					valid = false;
+					break;
+				}
+			}
+			if(!valid) { break; }
+		}
+		return valid;
 	}
 	
 	/**
