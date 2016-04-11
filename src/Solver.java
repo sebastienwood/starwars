788dca2a40934eb8643c30a1ddad6a20b5553ad1
sebastienwood <sebastien.henwood@gmail.com>
2016-04-11 10:16:05 +0200

    File reader - done

--- src/Solver.java
+++ src/Solver.java
@@ -20,9 +20,28 @@ public class Solver {
 	public Solver(String src) {
 		Scanner in;
 		try {
-			File file = new File("Instances",src);
+			File file = new File("rsc",src);
 			in = new Scanner(new FileReader(file));
-			System.out.println(in.nextLine());
+			/*
+			 * First line: nb night
+			 * Second: nb of obs
+			 * Then:
+			 * -carac obs
+			 * -for each night the time frame for this obst
+			 */
+			int nb_night = Tools.stringhandler_int(in.nextLine());
+			int nb_obs = Tools.stringhandler_int(in.nextLine());
+			this.etoiles = new Observation[nb_obs];
+			
+			//then we initialize each observation AKA star
+			for(int i = 0;i<nb_obs;i++) {
+				String obs_data = in.nextLine();
+				Night[] night_data = new Night[nb_night];
+				for(int j = 0; j<nb_night;j++) {
+					night_data[j] = Tools.stringhandler_night(in.nextLine());
+				}
+				this.etoiles[i] = Tools.stringhandler_observation(obs_data, night_data);
+			}
 			in.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
--- src/Tools.java
+++ src/Tools.java
@@ -1,7 +1,46 @@
+import java.util.*;
+
 /**
  * @author Sébastien
  *
  */
 public class Tools {
 
+	/**
+	 * A static method to return the int at the end of a string
+	 * @param line: the string to decipher
+	 * @return the int value at the end of the string
+	 */
+	public static int stringhandler_int(String line) {
+		return Integer.parseInt(line.substring(line.lastIndexOf(" ")+1));
+	}
+	
+	/**
+	 * A static method to return an observation given a string containing the raw observation and an array containing the data for each night of this observation
+	 * @param line: the line at the format Obs 0 priority : 30
+	 * @param data: an array with the data for all night concerning our observation
+	 * @return the finalized observation
+	 */
+	public static Observation stringhandler_observation(String line, Night[] data) {
+		int id = Integer.parseInt(line.substring(line.indexOf(" ")+1, line.indexOf("p")-1));
+		int priority = Integer.parseInt(line.substring(line.indexOf(":")+2, line.length()));
+		return new Observation(id, priority, data);
+	}
+	
+	/**
+	 * A static method to return a night given a string at the specified format
+	 * @param line: a line at the format "Night 57: p 78 r 417 m 475 d 525" OR "Night 58: "
+	 * duree debut meridien fin
+	 * @return a Night object
+	 */
+	public static Night stringhandler_night(String line) {
+		int id = Integer.parseInt(line.substring(line.indexOf(" ")+1, line.indexOf(":")));
+		int deb = 0;
+		int fin = 0;
+		if(line.indexOf(":")+1 != line.length()-1) {
+			deb = Integer.parseInt(line.substring(line.indexOf("r")+2, line.indexOf("m")-1));
+			fin = Integer.parseInt(line.substring(line.indexOf("d")+2, line.length()-1));
+		}
+		return new Night(id, deb, fin);
+	}
 }
